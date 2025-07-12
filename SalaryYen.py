f6b9724a9ec060836d945565c2406ae3f038a89c
Bvholz <Bvholz@gmail.com>
2025-07-12 22:52:03 +0900

    Add files via upload

---
+++ SalaryYen.py
@@ -0,0 +1,12 @@
+#Salary in Yen
+hourly_wage = 4000
+
+#Annual Salary
+print("Annual Salary is: ")
+print(hourly_wage * 40 * 50)
+print()
+
+#Monthly Salary
+print("Monthly salary is: ")
+print((hourly_wage * 40 * 50) / 12)
+print()
