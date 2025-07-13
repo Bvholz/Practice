9d55bd883550a153edfd732bc627320bf4ea34bf
Brandon <Bvholz@gmail.com>
2025-07-13 22:25:33 +0900

    Add files via upload

---
+++ MadLib.py
@@ -0,0 +1,31 @@
+#Mad Libs
+
+#Get user's words
+adjective = input('Enter an adjective: ')
+
+noun_plural = input('Enter a plural noun: ')
+
+verb_ing = input('Enter a verb ending in -ing: ')
+
+adverb = input('Enter an adverb: ')
+
+exclamation = input('Enter an exclamation: ')
+
+liquid = input('Enter a type of liquid: ')
+
+verb_past = input('Enter a verb in past tense: ')
+
+silly_word = input('Enter a silly word: ')
+
+number = input('Enter a number: ')
+
+celeb_name = input('Enter the name of a celebrity: ')
+print()
+
+#Tell the story
+print('Last weekend, I had the most', adjective, 'dream! I was surrounded by a group of talking',noun_plural,'.')
+print('They were all busy', verb_ing, 'and singing a song about me. It was all going so', adverb, 'until one of them shouted', exclamation,'!')
+print('Suddenly, a giant pitcher of', liquid, 'appeared out of nowhere. I',verb_past, 'towards it, but a giant', silly_word, 'blocked my path.')
+print('It told me I had to answer', number, 'riddles before I could take a sip.')
+print('Just as I was about to answer the first riddle,', celeb_name, 'floated down from the sky on a cloud and declared that weekends were for relaxing, not riddles. \nThen, we all danced the day away. I woke up feeling very confused but strangely refreshed.')
+
